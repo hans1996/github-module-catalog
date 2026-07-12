4f59adf5ceb80577af62c3337cd47277180871e3
Hans Mei <hans8512185121@gmail.com>
2026-07-13 05:57:39 +0800

    chore: scaffold github module catalog

---
+++ src/github_module_catalog/__init__.py
@@ -0,0 +1,9 @@
+"""GitHub Module Catalog package."""
+
+from typing import Final
+
+__version__: Final = "0.1.0"
+
+
+def main() -> None:
+    """Provide the console-script placeholder until the CLI is implemented."""
